f6493973ffcf4ef8ed4d86872ffccdc1d904ba4b
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2023-07-09 20:01:21 -0500

    DB

--- Methods/Database.php
+++ Methods/Database.php
@@ -1,3 +1,33 @@
 <?php 
+    function conectarBD($nombreBD) {
+        $servidor = "localhost";
+        $usuario = "root";
+        $password = "";
+    
+        // Crear la conexión
+        $conexion = new mysqli($servidor, $usuario, $password, $nombreBD);
+    
+        // Verificar la conexión
+        if ($conexion->connect_error) {
+            die("Error al conectar a la base de datos: " . $conexion->connect_error);
+        }
+    
+        // Establecer el juego de caracteres
+        $conexion->set_charset("utf8");
+    
+        return $conexion;
+    }
+    
+    function convertirResultadoEnArray($resultado) {
+        $arrayResultado = array();
+    
+        if ($resultado->num_rows > 0) {
+            while ($fila = $resultado->fetch_assoc()) {
+                $arrayResultado[] = $fila;
+            }
+        }
+    
+        return $arrayResultado;
+    }
     
 ?>
--- backEnd/regist_user.php
+++ backEnd/regist_user.php
@@ -1,3 +1,24 @@
 <?php 
-    
+    require '../Methods/Database.php';
+    $verify = [
+        (isset($_POST['email'])),
+        (isset($_POST['nombre'])),
+        (isset($_POST['usuario'])),
+        (isset($_POST['password'])),
+    ];
+    $Already=true;
+    for ($i=0; $i < count($verify); $i++) { 
+        if ($verify==false) {
+            $Already=false;
+        }
+    }
+    if ($Already) {
+        $conexion = conectarBD("app");
+        $sql = "SELECT * FROM usuarios";
+        $resultado = $conexion->query($sql);
+        $arrayResultado = convertirResultadoEnArray($resultado);
+        print_r($arrayResultado);
+        $conexion->close();
+    }
+
 ?>
