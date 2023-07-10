f67a98c342eb5cbdafc43d53f27615f0a43d3e19
José Daniel <96458028+Tsurematsu@users.noreply.github.com>
2023-07-09 21:22:22 -0500

    doc2

--- backEnd/validate.php
+++ backEnd/validate.php
@@ -1,29 +1,36 @@
 <?php
-    require '../Methods/tools.php';
-    require '../Methods/Database.php';
+    require '../Methods/tools.php'; // Se importa el archivo con funciones auxiliares
+    require '../Methods/Database.php'; // Se importa el archivo que contiene la lógica de conexión a la base de datos
+
     // Verificar si se han enviado los datos de inicio de sesión
     if (isset($_POST['username']) && isset($_POST['password'])) {
         // Obtener los valores ingresados por el usuario
         $usuario = $_POST['username'];
         $password = $_POST['password'];
+
+        // Construir la consulta SQL para verificar las credenciales del usuario
         $sql = "SELECT observaciones FROM usuarios WHERE usuario = '$usuario' AND password = '$password'";
+        
+        // Ejecutar la consulta para obtener el resultado
         $retorno = query($sql);
-        if (count($retorno)>0) {
-            $ruta = "frontEnd/welcome.html";
-            if ($retorno[0]['observaciones']=="admin") {
-                $ruta = "frontEnd/admin.html";
+
+        if (count($retorno)>0) { // Si se encontraron registros coincidentes
+            $ruta = "frontEnd/welcome.html"; // Ruta predeterminada para redireccionar al usuario
+
+            if ($retorno[0]['observaciones']=="admin") { // Si el usuario es administrador
+                $ruta = "frontEnd/admin.html"; // Se actualiza la ruta de redirección a la página de administrador
             }
-            // Las credenciales son correctas, redirection a la página de inicio
+
+            // Las credenciales son correctas, redirección a la página de inicio
             Redirection("../", [
                 ['page', $ruta],
                 ['username', $usuario]
             ]);
         } else {
-            //si las credenciales son incorrectas, redireccionar a la página de login
+            // Las credenciales son incorrectas, redirección a la página de login con un mensaje de error
             Redirection("../?page=login", [
                 ['msg', 'usuario o contraseña incorrecta'],
             ]);
         }
-
     }
 ?>
